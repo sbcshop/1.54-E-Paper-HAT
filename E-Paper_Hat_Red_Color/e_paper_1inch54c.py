05d6c0b0bd0e7c54fdc53ca11cd3921fef82606c
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-04-01 17:44:20 +0530

    Add files via upload

---
+++ E-Paper_Hat_Red_Color/e_paper_1inch54c.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import lib_1nch54c_e_paper
+import time
+from PIL import Image,ImageDraw,ImageFont
+
+
+try:    
+    e_paper = lib_1nch54c_e_paper.epaper()
+    e_paper.init()
+    #e_paper.Clear_screen()
+    time.sleep(1)
+    
+    # Drawing on the image
+    black_image = Image.new('1', (e_paper.width, e_paper.height), 255)  # 255: clear the frame
+    red_image = Image.new('1', (e_paper.width, e_paper.height), 255)  # 255: clear the frame
+    
+    font = ImageFont.truetype(('pic/Font.ttc'), 28)
+    font18 = ImageFont.truetype(('pic/Font.ttc'), 18)
+    
+    draw_black = ImageDraw.Draw(black_image)
+    draw_red = ImageDraw.Draw(red_image)
+    #draw_black.rectangle((0, 10, 200, 34), fill = 0)
+    draw_black.text((8, 12), 'hello world', font = font, fill = 0)
+
+    e_paper.display_image(e_paper.buffer(black_image),e_paper.buffer(red_image))
+    time.sleep(1)
+    
+    # read bmp file 
+    black_image = Image.open('pic/img1.bmp')
+    red_image = Image.open('pic/img2.bmp')    
+    e_paper.display_image(e_paper.buffer(black_image),e_paper.buffer(red_image))
+    time.sleep(1)
+    
+    e_paper.Clear_screen()
+    
+    e_paper.sleep()
+        
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    lib_1nch54c_e_paper.e_paper_config4.device_exit()
+    exit()
